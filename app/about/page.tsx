7962a0ab11c2e380bc0bdd6e139ea182e2cc6737
SHUN <admin@kuzhan.cn>
2024-10-17 21:37:20 +0800

    first commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 import { title } from "@/components/primitives";
-
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 export default function AboutPage() {
   return (
     <div>
+      <ConnectButton />
       <h1 className={title()}>About</h1>
     </div>
+    
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -5,6 +5,32 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+// 引入 rainbowkit star
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
+const config = getDefaultConfig({
+    appName: 'phantomBloc',
+    projectId: 'b5284eec2e9a2a680e1d7a86da84c5e0',
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+});
+// 引入 rainbowkit end
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -15,8 +41,15 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </NextUIProvider>
+    <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        <NextUIProvider navigate={router.push}>
+           <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        </NextUIProvider>
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
+    
   );
 }
