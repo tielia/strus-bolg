be264cbbc7940835fdc1fff54e8b34f5107dc9c7
SHUN <admin@kuzhan.cn>
2024-10-21 23:44:35 +0800

    dd

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 
 /** @type {import('next').NextConfig} */
-const nextTranslate = require('next-translate-plugin')
-const i18nConfig = require('./i18n')
+// const nextTranslate = require('next-translate-plugin')
+// const i18nConfig = require('./i18n')
 
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = {
-                ...config.resolve.fallback,
-                fs: false,
-            };
-        }
-        return config;
-    }
+    // webpack: (config, { isServer }) => {
+    //     if (!isServer) {
+    //         config.resolve.fallback = {
+    //             ...config.resolve.fallback,
+    //             fs: false,
+    //         };
+    //     }
+    //     return config;
+    // }
     // 其他Next.js配置
 }
 
-module.exports = nextTranslate(nextConfig)
+module.exports = nextConfig
